adc82bd0d8ed3d219078c79cb6028ea4ca515072
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-07-22 12:55:47 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var whatIsQWERTY = $(".whatIsQWERTY");
+var home = $(".home");
+var goHome = $(".goHome");
+
+whatIsQWERTY.click(function(){
+    location.replace("about.html");
+})
+
+home.click(function(){
+    location.replace("qwerty.html");
+})
+
+goHome.click(function(){
+    location.replace("index.html");
+})
